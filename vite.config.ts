5b66130dd628e6928fc3d010ee793383e382f2e6
medicocenterilheus <seu-email-da-clinica@exemplo.com>
2026-08-01 13:51:52 -0300

    Correção tela branca

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/portal_resultados/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
